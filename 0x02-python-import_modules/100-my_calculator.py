a8458da714e96b4d62bb745c649188ae6f97b981
hajoura89 <hajarkammou@gmail.com>
2023-07-13 08:37:29 -0700

    Task 6 Fixing Errors

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Handle basic operations
+
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
@@ -7,18 +7,12 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
 
-    op = {
-            "+": add, 
-            "-": sub, 
-            "*": mul, 
-            "/": div
-    }
-    if sys.argv[2] not in list(op.keys()):
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
