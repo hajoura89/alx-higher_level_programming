02cf4f5716b0e36b606c489a8d907f006cbadb55
hajoura89 <hajarkammou@gmail.com>
2023-07-31 14:25:07 -0700

    Tasks 2-3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Function that prints the first x elements
-    		of a list and only integers.
+    """Function that prints the first x elements of a list and only integers.
     Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     """
-    Function that divides 2 integers 
-    and prints the result.
+    Function that divides 2 integers and prints the result.
     """
     try:
         div = a / b
