c233d69b624626351685cc7a36911588f3a6294e
hajoura89 <hajarkammou@gmail.com>
2023-07-31 17:42:48 -0700

    fixing  error

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,8 @@
 #include <Python.h>
 #include <stdio.h>
+void print_python_float(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_list(PyObject *p);
 /**
  * print_python_list - gives data of the PyListObject
  * @p: the PyObject
