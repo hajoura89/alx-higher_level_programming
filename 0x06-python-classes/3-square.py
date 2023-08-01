354737cc2008e47ff3d4b36a1b473d45f9009122
hajoura89 <hajarkammou@gmail.com>
2023-08-01 16:04:22 -0700

    Fixing Errors

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,14 +10,14 @@ class Square:
         """Instantiation with size
 
         Args:
-            size (int): The size of the new square
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """Return the current square area"""
         return (self.__size * self.__size)
