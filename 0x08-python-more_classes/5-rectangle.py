6f32e4e86f36119fb0fde629011f9b408d582987
hajoura89 <hajarkammou@gmail.com>
2023-08-07 16:53:38 -0700

    Tasks4-5

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -14,10 +14,12 @@ class Rectangle:
         self.width = width
         self.height = height
 
+
     @property
     def width(self):
         """Get the width of the rectangle"""
         return self.__width
+
 
     @width.setter
     def width(self, value):
@@ -27,10 +29,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+
     @property
     def height(self):
         """Get the height of the rectangle"""
         return self.__height
+
 
     @height.setter
     def height(self, value):
@@ -40,16 +44,19 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
         
     def area(self):
         """The area of the Rectangle"""
         return (self.__width * self.__height)
+
 
     def perimeter(self):
         """The perimeter of the Rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
         
     def __str__(self):
         """Print the rectangle with the character # """
@@ -62,12 +69,14 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
         
     def __repr__(self):
         """The string representation of the Rectangle"""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
         
     def __del__(self):
         """Print Bye rectangle when a Rectangle is deleted"""
