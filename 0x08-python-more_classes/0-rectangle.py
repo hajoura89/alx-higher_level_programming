05d033a09d0c538e2624289000b876d72c9cd121
hajoura89 <hajarkammou@gmail.com>
2023-08-07 16:42:01 -0700

    Tasks0-3

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Empty class that defines a Rectangle"""
+
+
 class Rectangle:
     """Define a rectangle"""
+
     pass
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Empty class that defines a Rectangle"""
+
+
 class Rectangle:
     """Define a rectangle"""
+
     def __init__(self, width=0, height=0):
         """Instantiation with optional  width and height
         Args:
