0643c28a3e38d09a8c498840ae0f6ee4c4d9f8fb
hajoura89 <hajarkammou@gmail.com>
2023-08-07 17:02:20 -0700

    Tasks 5-6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,23 +3,29 @@
 
 
 class Rectangle:
-    """Define a rectangle"""
+    """Define a rectangle
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Instantiation with optional  width and height
+
         Args:
-            width (int): The width of the rectangle
-            height (int): The height of the rectangle
+            width (int): The width of the new rectangle
+            height (int): The height of the new rectangle
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
-
     @property
     def width(self):
-        """Get the width of the rectangle"""
+        """Get the width of the Rectangle"""
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -29,12 +35,10 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
-        """Get the height of the rectangle"""
+        """Get the height of the Rectangle"""
         return self.__height
-
 
     @height.setter
     def height(self, value):
@@ -44,11 +48,9 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-        
     def area(self):
         """The area of the Rectangle"""
         return (self.__width * self.__height)
-
 
     def perimeter(self):
         """The perimeter of the Rectangle"""
@@ -56,9 +58,8 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-        
     def __str__(self):
-        """Print the rectangle with the character # """
+        """Print the rectangle with the character #"""
         if self.__width == 0 or self.__height == 0:
             return ("")
 
@@ -69,14 +70,13 @@ class Rectangle:
                 rect.append("\n")
         return ("".join(rect))
 
-        
     def __repr__(self):
         """The string representation of the Rectangle"""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
 
-        
     def __del__(self):
         """Print Bye rectangle when a Rectangle is deleted"""
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
